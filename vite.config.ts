67a30057d883a0ff15b872fdfa21daf503bed755
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-09-25 16:20:15 -0400

    Remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const fallbackBasePath = '/train-car-logger/';
-
-const getBasePath = () => {
-  const repository = process.env.GITHUB_REPOSITORY;
-  if (!repository) return fallbackBasePath;
-
-  const [, repoName] = repository.split('/');
-  if (!repoName) return fallbackBasePath;
-
-  return `/${repoName}/`;
-};
-
-const repoBasePath = getBasePath();
-
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? repoBasePath : '/',
   plugins: [react()],
 }));
