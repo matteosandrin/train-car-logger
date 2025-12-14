3b2c5bda78141773dc02128230dbec781a86dc10
Matteo Sandrin <sandrin98@gmail.com>
2025-12-14 14:24:41 -0500

    Add label to taskbar button

--- src/components/ui/TaskBar.tsx
+++ src/components/ui/TaskBar.tsx
@@ -4,20 +4,22 @@ import { LuHash, LuHistory } from "react-icons/lu";
 
 const TaskBarButton: React.FC<{
   icon: React.ReactNode;
+  label: string;
   isActive: boolean;
   onClick: () => void;
-}> = ({ icon, isActive, onClick }) => {
+}> = ({ icon, label, isActive, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`flex flex-col w-28 items-center gap-1 px-6 py-4 rounded-full transition-all duration-100 ${
+      className={`flex flex-col items-center gap-1 w-[7rem] px-6 py-2 rounded-full transition-all duration-100 ${
         isActive
           ? "text-slate-900 bg-sky-100"
           : "text-slate-500 hover:bg-slate-100"
       }`}
       aria-current={isActive ? "page" : undefined}
     >
-      {icon}
+      <div className="w-6 h-6 flex items-center justify-center">{icon}</div>
+      <span className="text-sm font-medium">{label}</span>
     </button>
   );
 };
@@ -29,14 +31,16 @@ const TaskBar: React.FC = () => {
   return (
     <nav className="fixed bottom-0 left-0 right-0 pb-safe pb-8">
       <div className="bg-white/80 backdrop-blur-sm border border-slate-200 w-fit rounded-full shadow-lg mx-auto">
-        <div className="flex justify-center p-1">
+        <div className="flex justify-center p-1 gap-1">
           <TaskBarButton
-            icon={<LuHash className="w-6 h-6" />}
+            icon={<LuHash className="w-full h-full" />}
+            label="Log"
             isActive={location.pathname === "/"}
             onClick={() => navigate("/")}
           />
           <TaskBarButton
-            icon={<LuHistory className="w-6 h-6" />}
+            icon={<LuHistory className="w-full h-full" />}
+            label="History"
             isActive={location.pathname === "/log"}
             onClick={() => navigate("/log")}
           />
