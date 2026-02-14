10b0855aa5063944f56b4ab3a8c4500f24b1eca4
Matteo Sandrin <sandrin98@gmail.com>
2026-02-13 17:41:49 -0800

    Add pwa functionality with Serwist

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
-import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
 
-const nextConfig: NextConfig = {};
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
 
-export default nextConfig;
+export default withSerwist({});
